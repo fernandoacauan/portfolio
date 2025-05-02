d4d42b5a924923339272945f5dc6a3b077b44eeb
Fernando A <mcflyj038@gmail.com>
2025-05-02 16:06:56 -0300

    .

--- src/components/creditos/creditos.tsx
+++ src/components/creditos/creditos.tsx
@@ -1,7 +1,7 @@
 export default function Creditos() {
     return (
         <div className="footer">
-            Copyright (C) Fernando Acauan 2025.
+            <p>Copyright (C) Fernando Acauan 2025.</p>
         </div>
     );
 }
