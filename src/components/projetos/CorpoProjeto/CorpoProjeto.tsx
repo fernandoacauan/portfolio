4a8b231d0fe072a84340fb4f9a58acaf74a2b82b
Fernando A <mcflyj038@gmail.com>
2025-05-05 17:09:34 -0300

    .

--- src/components/projetos/CorpoProjeto/CorpoProjeto.tsx
+++ src/components/projetos/CorpoProjeto/CorpoProjeto.tsx
@@ -1,14 +1,12 @@
 import './CorpoProjeto.css'
-
+import Web1 from '../../../assets/calc.jpg'
 export default function Projetos() {
     return (
         <section id="projetos">
             <h1 className="projetos-section-title">WEB</h1>
             <div className="projetos-container">
                 <div className="projeto-card">
-                    <div className="projeto-img">
-                        {/* <img src={Web1} alt="projeto" /> se tiver imagem real */}
-                    </div>
+                    <img className="project-img" src={Web1} alt="projeto"></img>
                     <p className="projeto-text">
                         Site pessoal desenvolvido em React, com foco em UI/UX e responsividade.
                     </p>
