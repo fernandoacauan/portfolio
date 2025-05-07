8e7f4507b90a712df1c3d3487982c405dae80b73
Fernando A <mcflyj038@gmail.com>
2025-05-07 16:56:14 -0300

    .

--- src/components/contact/corpo/corpoContact.tsx
+++ src/components/contact/corpo/corpoContact.tsx
@@ -4,7 +4,16 @@ export default function CorpoContact()
 {
     return(
         <div className="body">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p>CONTACT ME</p>
+            <div className="dados">
+                <p>Nome:</p>
+                <input className="caixa"></input>
+                <p>Email:</p>
+                <input className="caixa"></input>  
+                <p>Message:</p>
+                <input className="caixa-message"></input> 
+            </div>
+            <button>ENVIAR</button>
         </div>
     );
 }
