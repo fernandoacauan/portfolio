662d2074b0a61fcdef0d6f5e50a1c937e3acae2d
Fernando <mcflyj038@gmail.com>
2025-05-09 01:05:45 -0300

    .

--- src/components/corpo/corpo.tsx
+++ src/components/corpo/corpo.tsx
@@ -1,12 +1,12 @@
 import './corpo.css';
 import Techs from '../techs/techs';
-
+import Foto from '../../assets/formatura.jpg'
 export default function Corpo() {
     return (
         <div className="corpo">
             <div className="corpoBanner">
                 <div className="apresentacao">
-                    <img className="foto-perfil" alt="Perfil" />
+                    <img className="foto-perfil" src={Foto} alt="Perfil" />
                     <div className="texto-apresentacao">
                         <h1>Fernando Acauan</h1>
                         <p>Computer Engineering student at PUCRS, specializing in desktop applications and embedded systems development.</p>
